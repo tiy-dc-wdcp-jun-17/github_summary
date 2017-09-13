e0f69f41caf7360d1d1457c3060d78bf06379dfd
Russell Osborne <russell@theironyard.com>
2017-09-13 13:55:05 -0400

    Add authenticated get method

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -21,7 +21,7 @@ class SummariesController < ApplicationController
 
   def get_repos(username)
     url = "#{GITHUB_API_URL}/users/#{username}/repos"
-    response = HTTParty.get(url)
+    response = authenticated_get(url)
     response.sort do |a, b|
       b["pushed_at"].to_date <=> a["pushed_at"].to_date
     end
@@ -34,5 +34,18 @@ class SummariesController < ApplicationController
       .uniq
       .sort
       .join(", ")
+  end
+
+  def authenticated_get(url)
+    token = ENV["GITHUB_PERSONAL_ACCESS_TOKEN"]
+    username = ENV["GITHUB_USERNAME"]
+
+    authenticated_get(
+      url,
+      headers: {
+        "Authorization": "token #{token}",
+        "User-Agent": username
+      }
+    )
   end
 end
